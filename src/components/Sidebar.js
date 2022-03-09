9ede2ae660fbc007222274f7bfb04fc7ca56f6ce
Henry Miller <mail.henrym+git@gmail.com>
2022-03-09 17:40:44 -0500

    removed unnecessary files

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { elastic as Menu } from 'react-burger-menu';
 import '../style/sidebar.css';
 import MiniCard from './MiniCard';
+import heart from '../img/real_heart.png';
 
 function Sidebar({matches,setMatches}) {
 
     let matchesToDisplay = matches.map(match=>{
         return (
             //update to fetch individual profiles
-            <MiniCard character={match} />
+            <MiniCard 
+                key={match.name}
+                character={match} />
         )
     })
 
@@ -21,9 +24,12 @@ function Sidebar({matches,setMatches}) {
             <a className="menu-item" href="/profile">
                 Profile
             </a> */}
-            <a className="large-menu-item" href="/matches">
-                Matches
-            </a>
+            <a className="large-menu-item" href="/">Home</a>
+            <div className='sidebar-title'> 
+                <a className="large-menu-item" href="/matches">Matches</a>
+                <img className="mini-heart" src={heart} />
+            </div>
+            
             {/* <a className="menu-item" href="/settings">
                 Settings
             </a> */}
